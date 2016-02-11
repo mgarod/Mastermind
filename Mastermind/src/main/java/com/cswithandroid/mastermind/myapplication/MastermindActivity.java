7200af6a332d281a3ee52586b516cfd4bed89343
Jonathan Galsurkar <jgalsurkar@gmail.com>
2016-02-11 15:26:52 -0500

    remove hidden files

--- Mastermind/src/main/java/com/cswithandroid/mastermind/myapplication/MastermindActivity.java
+++ Mastermind/src/main/java/com/cswithandroid/mastermind/myapplication/MastermindActivity.java
@@ -12,6 +12,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -254,6 +255,9 @@ public class MastermindActivity extends AppCompatActivity {
             attempts.add(userInput + ": " + Integer.toString(hits));
             TextView score = (TextView) findViewById(R.id.scoreBoard);
             score.setText(Integer.toString(++tries));
+
+            ListView listV = (ListView) findViewById(R.id.visibleWords);
+            
 
         }
 
